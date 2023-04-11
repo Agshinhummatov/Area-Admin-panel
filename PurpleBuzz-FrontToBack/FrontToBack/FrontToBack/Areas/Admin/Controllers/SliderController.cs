60833ec38abc7d2579a2f5279404757580be9649
Agshinhummatov <agshinshh@code.edu.az>
2023-04-11 06:16:52 +0400

    Homework

---
+++ PurpleBuzz-FrontToBack/FrontToBack/FrontToBack/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,69 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class SliderController : Controller
+    {
+
+
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            SliderBackground sliderBackground = await _context.sliderBackgrounds.Where(m => !m.SoftDelete).FirstOrDefaultAsync();
+
+
+            return View(sliderBackground);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            SliderBackground? sliderBackground = await _context.sliderBackgrounds.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (sliderBackground is null) return NotFound();
+
+            return View(sliderBackground);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            SliderBackground? sliderBackground = await _context.sliderBackgrounds.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (sliderBackground is null) return NotFound();
+
+            return View(sliderBackground);
+        }
+
+
+
+
+
+    }
+}
---
+++ PurpleBuzz-FrontToBack/FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            SliderBackground sliderBackground = await _context.sliderBackgrounds.Where(m => !m.SoftDelete).FirstOrDefaultAsync();
+
+            IEnumerable<Slider> sliders = await _context.Sliders.Where(m => !m.SoftDelete).ToListAsync();
+
+            IEnumerable<InfoService> infoServices = await _context.InfoServices.Where(m => !m.SoftDelete).ToListAsync();
+
+            IEnumerable<Category> categories = await _context.Categories.Where(m => m.SoftDelete == false).ToListAsync();
+
+            IEnumerable<Work> works = await _context.Works.Where(m => m.SoftDelete == false).Include(m => m.WorkImages).Include(m => m.Category).ToListAsync();
+
+            IEnumerable<RecentWork> recentWorks = await _context.RecentWorks.Where(m => m.SoftDelete == false).ToListAsync();
+
+
+            HomeVM model = new()
+            {
+                Sliders = sliders,
+                InfoServices = infoServices,
+                Categories = categories,
+                Works = works,
+                RecentWorks = recentWorks,
+                sliderBackground = sliderBackground
+
+            };
+
+            return View(model);
+        }
+
+       
+
+     
+    }
+}
